fc483771d755ad558b087005b8d4138392a18c50
leopaglia <paglialungalf@gmail.com>
2017-02-19 23:08:21 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/models/apartment.model.ts
@@ -0,0 +1,13 @@
+export class Apartment {
+  price: number;
+  rooms: number;
+  address: string;
+  status: string;
+  location: string;
+  covered_area: number;
+  total_area: number;
+  requires_guarranty: boolean;
+  url: string;
+  phone: string;
+  email: string;
+}
